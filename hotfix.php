3d1f8020ef6a3b810ae5a51ac1bf8f1cfbc321e7
mt8 <regist@mt8.biz>
2018-05-16 22:47:41 +0900

    fix mysql_list_tables()はmysqliにないため代替え関数を用意

--- hotfix.php
+++ hotfix.php
@@ -6,3 +6,11 @@ $HTTP_GET_VARS = $_GET;
 $HTTP_POST_VARS = $_POST;
 $HTTP_COOKIE_VARS = $_COOKIE;
 $HTTP_SERVER_VARS = $_SERVER;
+
+if ( ! function_exists('mysqli_list_tables' ) ) :
+	//mysql_list_tablesの互換関数
+	function mysqli_list_tables( $DB_NAME ) {
+		global $wpdb;
+		return mysqli_query($wpdb->dbh,"SHOW TABLES");
+	}
+endif;
